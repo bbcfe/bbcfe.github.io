e6809873f9d474f78169c80005ea6886c1b7656e
nightninja778 <58561691+nightninja778@users.noreply.github.com>
2020-11-23 22:50:51 +0000

    __

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-	
+	console.log('Function loaded');
 	$('.menu-Toggle').click(function() {
 		$('.menu-Toggle').toggleClass('active')
 		$('.menu').toggleClass('active')
