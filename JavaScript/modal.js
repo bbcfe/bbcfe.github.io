7bc1f547203f0c7a337398ffa2ee518092dfaafa
nightninja778 <58561691+nightninja778@users.noreply.github.com>
2020-12-16 12:21:47 +0000

    Fixed home modal

--- JavaScript/modal.js
+++ JavaScript/modal.js
@@ -1,18 +1,19 @@
-var modal = document.getElementsByClassName("modal")[0];
+
 
 setInterval(() => {
-$('head').append('<link rel="stylesheet" href="/CSS/modal.css">');
-// $('body').append('<div id="myModal"></div>');
-var button = document.getElementById("modalBtn");
+	var modal = document.getElementsByClassName("modal")[0];
+	$('head').append('<link rel="stylesheet" href="/CSS/modal.css">');
+	// $('body').append('<div id="myModal"></div>');
+	var button = document.getElementById("modalBtn");
 
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function(){
-	modal.style.display = "none";
-}
+	var span = document.getElementsByClassName("close")[0];
+	span.onclick = function(){
+		modal.style.display = "none";
+	}
 
-button.onclick = function() {
-	modal.style.display = "block";
-}
-}, 20);
+	button.onclick = function() {
+		modal.style.display = "block";
+	}
+}, 50);
 
 
